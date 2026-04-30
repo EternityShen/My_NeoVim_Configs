9fe92c727d0929602dceda25b19505c4550b5ef3
EternityShen <3258551611@qq.com>
2026-04-30 20:10:23 +0800

    fix:Neovim 在调用 nvim_buf_set_extmark 时报错的问题

--- lua/plugins/inkay-hints.lua
+++ lua/plugins/inkay-hints.lua
@@ -6,16 +6,25 @@
 return {
   -- 行内类型提示插件仓库
   "MysticalDevil/inlay-hints.nvim",
-  
+
   -- 在 LSP 附加到缓冲区时加载
   event = "LspAttach",
-  
+
   -- 依赖项：LSP 配置插件
   dependencies = { "neovim/nvim-lspconfig" },
-  
+
   -- 插件配置函数
   config = function()
     -- 使用默认配置初始化行内类型提示
+    local orig_set = vim.lsp.inlay_hint.set
+    vim.lsp.inlay_hint.set = function(...)
+      local ok, err = pcall(orig_set, ...)
+      if not ok then
+        if not err:match("out of range") then
+          vim.notify(err, vim.log.levels.ERROR)
+        end
+      end
+    end
     require("inlay-hints").setup()
   end
 }
